dae62d14826a35801262303e11a0c3af2108926c
CallmeElodie <108621982+CallmeElodie@users.noreply.github.com>
2024-11-12 21:16:49 -0600

    Pages and Navigation

--- nav.js
+++ nav.js
@@ -1,3 +1,9 @@
 function home(){
     window.location.replace("https://www.synthia.tech")
 }
+function getintouch(){
+    window.location.replace("https://www.synthia.tech/getintouch")
+}
+function portfolio(){
+    window.location.replace("https://www.synthia.tech/portfolio")
+}
