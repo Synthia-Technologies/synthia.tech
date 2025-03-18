73cb98039cd8b200c55e9cdad6144d1481658b4c
CallmeElodie <lfam5718@gmail.com>
2025-03-17 23:06:01 -0500

    nav.js optimization

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,13 @@
-
-export function home(){
+// yo I need to commit
+export function home() {
     window.location.replace("")
     document.getElementById('pageTitle').innerText = 'Synthia Technologies'
 }
-export function getintouch(){
+export function getintouch() {
     window.location.replace("/#/getintouch")
     document.getElementById('pageTitle').innerText = 'Synthia Technologies - Get in Touch'
 }
-export function portfolio(){
+export function portfolio() {
     window.location.replace("/#/portfolio")
     document.getElementById('pageTitle').innerText = 'Synthia Technologies - Portfolio'
 }
